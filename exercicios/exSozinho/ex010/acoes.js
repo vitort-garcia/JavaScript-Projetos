3620134eac1158d4824059f1359ec43306ec4859
Vitor Garcia Teixeira <90659838+vitort-garcia@users.noreply.github.com>
2022-03-07 13:24:47 -0300

    mais coisa

---
+++ exercicios/exSozinho/ex010/acoes.js
@@ -0,0 +1,17 @@
+var acao_ = document.querySelector('p#register')
+
+function clicou1(){
+    acao_.innerHTML = (acao_.innerHTML + '<p>Clicou no primeiro botão</p>')
+}
+
+function clicou2(){
+    acao_.innerHTML = (acao_.innerHTML + '<p>Clicou no segundo botão</p>')
+}
+
+function clicou3(){
+    acao_.innerHTML = (acao_.innerHTML + '<p>Clicou no terceiro botão</p>')
+}
+
+function clicou4(){
+    acao_.innerHTML = (acao_.innerHTML + '<p>Clicou no quarto botão</p>')
+}

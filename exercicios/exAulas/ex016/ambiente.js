89e4e72a522260210967512b938c525caeb3adea
Vitor Garcia Teixeira <90659838+vitort-garcia@users.noreply.github.com>
2022-03-15 05:33:30 -0300

    testando as estruturas de repetições

---
+++ exercicios/exAulas/ex016/ambiente.js
@@ -0,0 +1,24 @@
+for(var cont = 1;cont<=10; cont++){
+    console.log(`Tudo bem? ${cont}`)
+}
+
+/*var cont = 1
+do{
+    console.log(`Tudo bem? ${cont}`)
+    cont++
+} while(cont <= 5)
+*/
+
+/*var cont = 1
+while (cont <= 5){
+    console.log(`Tudo bem? ${cont}`)
+    cont++
+}*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/

e065346a23b7a462db7d0823e04bbce6e45db58c
Vitor Garcia Teixeira <90659838+vitort-garcia@users.noreply.github.com>
2022-03-15 01:58:12 -0300

    mudei nada

--- exercicios/exSozinho/aulaex015/script.js
+++ exercicios/exSozinho/aulaex015/script.js
@@ -2,45 +2,40 @@ function verificar(){
     var ano_atual = new Date().getFullYear()
     var ano_nasc = document.getElementById('ano')
     ano_nasc = ano_nasc.value
-    var idade = ano_atual - ano_nasc
+    idade = ano_atual - ano_nasc
     var resul = document.getElementById('result')
-    
-    if(idade < 0 || idade > 119){
+
+    if(idade > 119){
         alert('[ERRO] Verifique os dados e tente novamente [ERRO]')
     } else{
+
         var genero = ''
         var radiores = document.getElementsByName('radsex')
-        var image = document.createElement('img')
-        
+        var imagem = document.createElement('img')
         if(radiores[0].checked){
             genero = 'homem'
-            
-            if(idade <= 10){
-                image.setAttribute('src', 'criancaH.png')
+            if(idade <= 11){
+                imagem.setAttribute('src', 'criancaH.png')
             } else if(idade <= 19){
-                image.setAttribute('src', 'jovemH.png')
+                imagem.setAttribute('src', 'jovemH.png')
             } else if(idade <= 59){
-                image.setAttribute('src', 'adultoH.png')
+                imagem.setAttribute('src', 'adultoH.png')
             } else{
-                image.setAttribute('src', 'idosoH.png')
+                imagem.setAttribute('src', 'idosoH.png')
             }
-
-        } else if(radiores[1].checked){
-            genero = 'mulher'
-
-            if(idade <= 10){
-                image.setAttribute('src', 'criancaM.png')
+        } else{
+            if(idade <= 11){
+                imagem.setAttribute('src', 'criancaM.png')
             } else if(idade <= 19){
-                image.setAttribute('src', 'jovemM.png')
+                imagem.setAttribute('src', 'jovemM.png')
             } else if(idade <= 59){
-                image.setAttribute('src', 'adultaM.png')
+                imagem.setAttribute('src', 'adultaM.png')
             } else{
-                image.setAttribute('src', 'idosaM.png')
+                imagem.setAttribute('src', 'idosaM.png')
             }
         }
-    
         resul.style.textAlign = 'center'
-        resul.innerHTML = `<p>Detectamos ${genero} com ${idade} anos</p>`
-        resul.appendChild(image)
+        resul.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+        resul.appendChild(imagem)
     }
 }
